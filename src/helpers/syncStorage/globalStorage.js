996f5d964305efd44aa280b4a156ff0fd40a5ae6
DmytroVoitovych <begunec23@gmail.com>
2023-03-06 23:35:51 +0200

    test домен

--- src/helpers/syncStorage/globalStorage.js
+++ src/helpers/syncStorage/globalStorage.js
@@ -3,6 +3,6 @@ import PouchDBFind from 'pouchdb-find';
 PouchDB.plugin(PouchDBFind);
 
 const localDB = new PouchDB('mydb');
-const remoteDB = new PouchDB('http://localhost:3000/mytoken');
+const remoteDB = new PouchDB('https://vue-filmoteka.vercel.app/mytoken');
 
 export { localDB, remoteDB };
