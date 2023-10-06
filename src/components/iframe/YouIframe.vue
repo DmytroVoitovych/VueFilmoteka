dd98305396f638c1c7b8dc2dd1f666e2956aeee7
DmytroVoitovych <begunec23@gmail.com>
2023-10-06 21:23:46 +0300

    finish rewriting component Youiframe to ts and composAPI

--- src/components/iframe/YouIframe.vue
+++ src/components/iframe/YouIframe.vue
@@ -2,33 +2,29 @@
   <div>
     <VueYtframe
       :player-vars="playerVars"
-      :videoId="getOfficialTreiler ?? video[0].key"
+      :videoId="getOfficialTreiler ?? video[0]?.key"
       ref="yt"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
-
-interface Props { video: [] | string[], playerVars: { autoplay: number, listType: string }  };
+interface Props { video: [] | {[key:string]:string}[], playerVars: { autoplay: number, listType: string }  };
 const props = withDefaults(defineProps<Props>(), { video:()=> [], playerVars: () => ({ autoplay: 0, listType: 'user_uploads' }) });
 const yt = ref<any>(null);
 
-// console.log(yt?._value ,'yt');
 const exitFrame = ()=>yt?.value?.stopVideo();
 const runFrame =  ()=>yt?.value?.playVideo();
 
-defineExpose({
+defineExpose({  // даю доступ батьківському компоненту
   runFrame,
   exitFrame
 });
-  // computed: {
-  //   getOfficialTreiler() {
-  //     return this.video.toReversed().find(e => e.official)?.key;
-  //   },
-  // },
+
+const treiler: string | undefined = props.video.slice().reverse().find(e => e.official)?.key;
+const getOfficialTreiler = computed<string | undefined>(() => treiler);
 
 </script>
 
